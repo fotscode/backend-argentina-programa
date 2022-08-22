1f572bea8e37e0680958ec7ada2ed31adca496bb
fots <facundotomatis@gmail.com>
2022-08-21 22:22:19 -0300

    changed update mapping to put instead of post

--- src/main/java/com/fots/backendap/api/ExperienceController.java
+++ src/main/java/com/fots/backendap/api/ExperienceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -79,7 +80,7 @@ public class ExperienceController {
         );
     }
 
-    @PostMapping("/update")
+    @PutMapping("/update")
     public ResponseEntity<Response> updateExperience(@RequestBody Experience experience) {
         return ResponseEntity.ok(
                 Response.builder()
