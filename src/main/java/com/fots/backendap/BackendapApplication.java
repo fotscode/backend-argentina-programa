0a467f948347c2e270afcd4bef596ca9a7f30c60
fots <facundotomatis@gmail.com>
2022-08-24 14:13:17 -0300

    added roles and admin user

--- src/main/java/com/fots/backendap/BackendapApplication.java
+++ src/main/java/com/fots/backendap/BackendapApplication.java
@@ -1,17 +1,14 @@
 package com.fots.backendap;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.fots.backendap.model.AppUser;
-import com.fots.backendap.model.Profile;
 import com.fots.backendap.model.Role;
 import com.fots.backendap.repositories.ProfileRepo;
 import com.fots.backendap.service.UserService;
@@ -31,8 +28,9 @@ public class BackendapApplication {
   @Bean
   CommandLineRunner run(UserService userService, ProfileRepo profileRepo) {
     return args -> {
-      //userService.saveRole(new Role(null, "ROLE_USER"));
-      //userService.saveUser(new AppUser(null, "admin", "1234", new ArrayList<>()));
+      Role user=new Role(null, "ROLE_USER");
+      userService.saveRole(user);
+      userService.saveUser(new AppUser(null, "admin", "1234", Arrays.asList(user)));
       //userService.addRoleToUser("admin", "ROLE_USER");
       //profileRepo.save(new Profile(null,"imagen1".getBytes(),"imagen2".getBytes(),"Nombre","Titulo","Descripcion"));
     };
