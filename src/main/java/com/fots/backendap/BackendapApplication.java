ac65e921c1f43344b4925c4f5dc51fb0132c3480
fots <facundotomatis@gmail.com>
2022-08-25 19:03:18 -0300

    removes users and roles before saving

--- src/main/java/com/fots/backendap/BackendapApplication.java
+++ src/main/java/com/fots/backendap/BackendapApplication.java
@@ -1,6 +1,6 @@
 package com.fots.backendap;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.fots.backendap.model.AppUser;
 import com.fots.backendap.model.Role;
 import com.fots.backendap.repositories.ProfileRepo;
+import com.fots.backendap.repositories.RoleRepo;
+import com.fots.backendap.repositories.UserRepo;
 import com.fots.backendap.service.UserService;
 
 @SpringBootApplication()
@@ -26,10 +28,13 @@ public class BackendapApplication {
   }
 
   @Bean
-  CommandLineRunner run(UserService userService, ProfileRepo profileRepo) {
+  CommandLineRunner run(UserService userService, UserRepo userRepo, RoleRepo roleRepo,ProfileRepo profileRepo) {
     return args -> {
-      //userService.addRoleToUser("admin", "ROLE_USER");
-      //profileRepo.save(new Profile(null,"imagen1".getBytes(),"imagen2".getBytes(),"Nombre","Titulo","Descripcion"));
+      userRepo.deleteAll();
+      roleRepo.deleteAll();
+      userService.saveRole(new Role(null, "ROLE_USER"));
+      userService.saveUser(new AppUser(null, "admin", "1234", new ArrayList<>()));
+      userService.addRoleToUser("admin", "ROLE_USER");
     };
   }
 }
